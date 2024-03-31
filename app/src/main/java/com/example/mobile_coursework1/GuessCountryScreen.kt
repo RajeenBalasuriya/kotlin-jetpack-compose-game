5c0db9778dde434dba510eaaf15cdef961655897
Rajeen Balasuriya <102136700+RajeenBalasuriya@users.noreply.github.com>
2024-03-31 14:45:07 +0530

    implement-reading-json-file

--- app/src/main/java/com/example/mobile_coursework1/GuessCountryScreen.kt
+++ app/src/main/java/com/example/mobile_coursework1/GuessCountryScreen.kt
@@ -1,9 +1,35 @@
 package com.example.mobile_coursework1
 
-import androidx.compose.material3.Text
+
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+
 
 @Composable
 fun GuessCountry(){
-    Text(text = "HELLO")
+        //reading values from the json file 
+        val jsonString=readJsonFile()
+
+
+
 }
+
+@Composable
+private fun readJsonFile(): String {
+    val context = LocalContext.current
+
+    //return jsonString
+    return context.assets
+        .open("countries.json")
+        .bufferedReader()
+        .use { it.readText() }
+
+
+}
+
+//data class
+data class CountryJson(
+    val letterCode:String,
+    val countryName:String
+)
+
