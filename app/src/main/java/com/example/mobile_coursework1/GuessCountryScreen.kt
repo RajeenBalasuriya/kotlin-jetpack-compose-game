00b9d13ed8dfca277c3cd2a836d22050edfecd98
Rajeen Balasuriya <102136700+RajeenBalasuriya@users.noreply.github.com>
2024-03-31 15:25:47 +0530

    created-country-objs

--- app/src/main/java/com/example/mobile_coursework1/GuessCountryScreen.kt
+++ app/src/main/java/com/example/mobile_coursework1/GuessCountryScreen.kt
@@ -3,12 +3,28 @@ package com.example.mobile_coursework1
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import org.json.JSONArray
+import org.json.JSONObject
 
 
 @Composable
 fun GuessCountry(){
-        //reading values from the json file 
+
+
+        //reading values from the json file
         val jsonString=readJsonFile()
+
+        //mutable list to store country objects
+        val countryList= mutableListOf<CountryJson>()
+
+        val jsonObject = JSONObject(jsonString)
+
+    // Iterate through each key-value pair in the JSON object to make country objects
+    jsonObject.keys().forEach { key ->
+        val countryName = jsonObject.getString(key)
+        val countryJson = CountryJson(key, countryName)
+        countryList.add(countryJson)
+    }
 
 
 
